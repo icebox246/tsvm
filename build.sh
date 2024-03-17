82bc6e8d99400d8ce4b9e498faefd58afa50d079
icebox246 <iceboxpoczta@gmail.com>
2024-03-17 21:18:01 +0100

    Make compilation parallel

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-gcc tsvm-ass.c -o tsvm-ass -ggdb
-gcc tsvm-run.c -o tsvm-run -ggdb
-gcc tsvm-gensyntax.c -o tsvm-gensyntax -ggdb
+gcc tsvm-ass.c -o tsvm-ass -ggdb &
+gcc tsvm-run.c -o tsvm-run -ggdb &
+gcc tsvm-gensyntax.c -o tsvm-gensyntax -ggdb &
+wait
 
 [ -d vim/syntax ] || mkdir vim/syntax
 ./tsvm-gensyntax
